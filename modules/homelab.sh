5191f202272708a0d79e6c3ff0a2cb1c1d3d8771
Matthew Jordan <mdjordan@gmail.com>
2026-07-30 14:11:24 -0500

    Complete LinuxBook workstation setup

---
+++ modules/homelab.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt update
+
+sudo apt install -y \
+    tmux \
+    git \
+    curl \
+    wget \
+    unzip \
+    zip \
+    tree \
+    htop \
+    btop \
+    ncdu \
+    ripgrep \
+    fd-find \
+    bat \
+    eza \
+    fzf \
+    jq \
+    yq \
+    rsync \
+    screen \
+    minicom \
+    picocom \
+    avahi-utils \
+    nmap \
+    iperf3 \
+    mosh \
+    dnsutils \
+    net-tools \
+    traceroute \
+    whois \
+    tcpdump \
+    wireshark \
+    ffmpeg \
+    imagemagick \
+    vlc
